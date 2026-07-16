64d1aa2884e05cb911d39143aa95c1c166b22b09
Hubert Grossin <hubert.grossin@gmail.com>
2026-07-16 18:23:17 +0200

    feat: restored original "Scavengers" Unity tutorial

---
+++ Samples~/Scavengers/Scripts/Loader.cs
@@ -0,0 +1,28 @@
+#pragma warning disable IDE1006 // Naming Styles, disabled for demo
+using UnityEngine;
+
+namespace SideXP.Broadcaster.Scavengers
+{
+    public class Loader : MonoBehaviour
+    {
+        public GameObject gameManager;            //GameManager prefab to instantiate.
+        public GameObject soundManager;            //SoundManager prefab to instantiate.
+
+
+        void Awake()
+        {
+            //Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null
+            if (GameManager.instance == null)
+
+                //Instantiate gameManager prefab
+                Instantiate(gameManager);
+
+            ////Check if a SoundManager has already been assigned to static variable SoundManager.instance or if it's still null
+            //if (SoundManager.instance == null)
+
+            //    //Instantiate SoundManager prefab
+            //    Instantiate(soundManager);
+        }
+    }
+}
+#pragma warning restore IDE1006 // Naming Styles
